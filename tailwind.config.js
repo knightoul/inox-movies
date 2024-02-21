889a7129f5d1b889da536d74c8544f881c7d6ec2
ARNAB KUMAR DAS - S0202316 <akdas2001.rkmscc@gmail.com>
2024-02-21 15:33:50 +0530

    add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#c800ff",
+
+          secondary: "#0034ff",
+
+          accent: "#ff4e00",
+
+          neutral: "#392527",
+
+          "base-100": "#fffcea",
+
+          info: "#00d6ff",
+
+          success: "#69db00",
+
+          warning: "#ffaf00",
+
+          error: "#ff5255",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
