2df718f9a17b3a2780569e3fc736823328f298d1
Sayan Das <sayanharipur3@gmail.com>
2024-02-23 20:18:08 +0530

    remove unused space

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,21 +9,13 @@ module.exports = {
       {
         mytheme: {
           primary: "#c800ff",
-
           secondary: "#0034ff",
-
           accent: "#ff4e00",
-
           neutral: "#392527",
-
           "base-100": "#fffcea",
-
           info: "#00d6ff",
-
           success: "#69db00",
-
           warning: "#ffaf00",
-
           error: "#ff5255",
         },
       },
