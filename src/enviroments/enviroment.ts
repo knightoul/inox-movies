8e1d59ce6c76e9900f888d146ce3ea541e9ab8a1
arnab kumar das <akdas2001.rkmscc@gmail.com>
2024-02-23 23:09:18 +0530

    update dev enviroment.
    remove personal development firebase config.

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,12 +1,15 @@
 export const enviroment = {
   firebaseConfig: {
-    apiKey: 'AIzaSyDg5YK_zAeeC-SLvXgYT5x0ctbNUUD13BU',
-    authDomain: 'inox-movies.firebaseapp.com',
-    databaseURL: 'https://inox-movies-default-rtdb.firebaseio.com',
-    projectId: 'inox-movies',
-    storageBucket: 'inox-movies.appspot.com',
-    messagingSenderId: '597133192945',
-    appId: '1:597133192945:web:e1792a85d0616767bd4350',
-    measurementId: 'G-K2P7CNYXS0',
+    /*
+    go to firebase console. create new project. and get this config.
+    */
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
